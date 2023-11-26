992ad18af9b1548f942c4b154723d01fe6b5bede
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2023-11-26 16:34:44 +1100

    Initial

---
+++ Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <h4>Hello</h4>
+        <h2>nice</h2>
+      </nav>
+    </header>
+  );
+}
+export default Header;
